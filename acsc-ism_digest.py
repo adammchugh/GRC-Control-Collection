47d8cfbb0b5d76fd4b2a0ca2de6a1db1721dc04f
Adam McHugh <adam@mchughsecurity.com>
2022-05-03 21:42:52 +0930

    Minor update

--- acsc-ism_digest.py
+++ acsc-ism_digest.py
@@ -1,16 +1,16 @@
 import os
 import hashlib
 
-digest_f = open('./README.md', 'w')
+digest_f = open('ACSC-ISM/README.md', 'w')
 digest_f.write('# ISM Revision Digests\n')
 digest_f.write('| Revision | MD5       |\n')
 digest_f.write('|----------|-----------|\n')
 
-directory = os.fsencode('.')
+directory = os.fsencode('./ACSC-ISM/')
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith('.xml'):
-        file_md5 = hashlib.md5(open(filename,'rb').read()).hexdigest()
+        file_md5 = hashlib.md5(open('./ACSC-ISM/' + filename,'rb').read()).hexdigest()
         print(filename + ' | ' + file_md5)
         digest_f.write('| ' + filename + '  | ' + file_md5 + ' |\n')
 
